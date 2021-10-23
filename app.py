b02db98b5cd8720ca61e70e489013347f4c48eea
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-10-22 22:10:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -348,7 +348,7 @@ for p in range(0,len(preds['Predicted_Difference'])):
 
 final = preds[['Visitor', 'Home', 'Team_Spread', 'Model Prediction', 'Spread_Beater' ]]
 final = final.rename(columns = {'Team_Spread': 'Spread', 'Spread_Beater': 'Winner Against Spread'})
-
+st.table(final)
 
 # In[36]:
 
