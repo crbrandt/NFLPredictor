69b09cac12cc273a023104239520cb0dad2a1777
crbrandt <83561834+crbrandt@users.noreply.github.com>
2021-12-11 13:19:42 -0800

    Update app.py

--- app.py
+++ app.py
@@ -146,8 +146,8 @@ with col_title:
   st.title('NFL Game Predictor')
   st.markdown(' ## Created by Cole Brandt')
   st.markdown('  Last updated: Sunday, December 5th, 2021')  
-  accuracy('Lifetime ATS accuracy: 57%')
-  accuracy('Lifetime ML accuracy: 67.3%')
+  accuracy('Lifetime ATS accuracy: 58.2%')
+  #accuracy('Lifetime ML accuracy: 67.3%')
 with col_logo:
   st.image("https://static.wikia.nocookie.net/logopedia/images/b/bc/NationalFootballLeague_PMK01a_1940-1959_SCC_SRGB.png")
 st.write("#")
